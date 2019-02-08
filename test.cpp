748f62e342a47adebaa5acab996ea3181ff44e2e
tlai23 <47377033+tlai23@users.noreply.github.com>
2019-02-07 20:17:28 -0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,7 +1,25 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-int main()
-{
-cout<<"Hello world"<<endl;
-return 0;
-}
+
+  int main()
+  {
+   int a[4]={1,2,3,4},b[4],i,j;
+   cout<<"Before: ";
+   for(i=0; i<4 ;i++)
+   {
+   cout<<a[i]<<" ";
+   }
+   cout<<endl;
+   cout<<"After: ";
+
+   for(i=3,j=0; i>=0;i--,j++)
+   {
+    b[i]=a[j];
+   }
+   for(i=0; i<4 ;i++)
+   {
+   cout<<b[i]<<" ";
+   }
+   cout<<endl;
+   return 0;
+  }
