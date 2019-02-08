e08fa83e641db9b37ef4f96e270339aa0f8f3d6a
tlai23 <47377033+tlai23@users.noreply.github.com>
2019-02-07 20:31:48 -0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -3,23 +3,23 @@ using namespace std;
 
   int main()
   {
-   int a[4]={1,2,3,4},b[4],i,j;
-   cout<<"Before: ";
-   for(i=0; i<4 ;i++)
-   {
-   cout<<a[i]<<" ";
-   }
-   cout<<endl;
-   cout<<"After: ";
+  int a[4]={1,2,3,4},b[4],i,j;
+  cout<<"Before: ";
+  for(i=0; i<4 ;i++)
+  {
+  cout<<a[i]<<" ";
+  }
+  cout<<endl;
+  cout<<"After: ";
 
-   for(i=3,j=0; i>=0;i--,j++)
-   {
-    b[i]=a[j];
-   }
-   for(i=0; i<4 ;i++)
-   {
-   cout<<b[i]<<" ";
-   }
-   cout<<endl;
-   return 0;
+  for(i=3,j=0; i>=0;i--,j++)
+  {
+  b[i]=a[j];
+  }
+  for(i=0; i<4 ;i++)
+  {
+  cout<<b[i]<<" ";
+  }
+  cout<<endl;
+  return 0;
   }
